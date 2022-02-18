a3e490689aa23ca8123a43e3754a7634dc11f5f8
pseudozach <zozsoy@gmail.com>
2022-02-18 08:11:36 +0000

    add price feeds

--- lib/rates/data/exchanges/Binance.ts
+++ lib/rates/data/exchanges/Binance.ts
@@ -5,12 +5,13 @@ class Binance implements Exchange {
 
   public async getPrice(baseAsset: string, quoteAsset: string): Promise<number> {
     console.log('binance getPrice ', baseAsset, quoteAsset);
-    quoteAsset = quoteAsset ? 'USD' : 'USDT'
+    // quoteAsset = quoteAsset ? 'USD' : 'USDT'
     if (baseAsset === 'CELO' && quoteAsset === 'BTC') {
       // get btcusdt and celousdt and divide
       const response1 = await makeRequest(`${Binance.API}/ticker/price?symbol=CELOUSDT`);
       const response2 = await makeRequest(`${Binance.API}/ticker/price?symbol=BTCUSDT`);
-      return Number(response1.price/response2.price);
+      console.log('binance returning ', response1.price/response2.price)
+      return Number(parseFloat(response1.price)/parseFloat(response2.price));
     } else {
       const response = await makeRequest(`${Binance.API}/ticker/price?symbol=${baseAsset.toUpperCase()}${quoteAsset.toUpperCase()}`);
       return Number(response.price);
--- lib/rates/data/exchanges/CoinbasePro.ts
+++ lib/rates/data/exchanges/CoinbasePro.ts
@@ -4,13 +4,11 @@ class CoinbasePro implements Exchange {
   private static readonly API = 'https://api.pro.coinbase.com';
 
   public async getPrice(baseAsset: string, quoteAsset: string): Promise<number> {
-    console.log('1CoinbasePro getPrice ', baseAsset, quoteAsset);
-    baseAsset = baseAsset === 'CELO' ? 'CGLD' : baseAsset
-    console.log('2CoinbasePro getPrice ', baseAsset, quoteAsset);
     if(baseAsset === 'CELO' && quoteAsset === 'BTC') {
       const response1 = await makeRequest(`${CoinbasePro.API}/products/CGLD-USD/ticker`);
       const response2 = await makeRequest(`${CoinbasePro.API}/products/BTC-USD/ticker`);
-      return Number(response1.price/response2.price);
+      console.log('coinbase returning ', response1.price/response2.price)
+      return Number(parseFloat(response1.price)/parseFloat(response2.price));
     } else {
       const response = await makeRequest(`${CoinbasePro.API}/products/${baseAsset}-${quoteAsset}/ticker`);
       return Number(response.price);
