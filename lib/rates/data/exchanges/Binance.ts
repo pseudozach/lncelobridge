4060e0562d58d47c00a3799faaef8a527c69f4f5
pseudozach <zozsoy@gmail.com>
2022-02-18 07:57:06 +0000

    add celo price feeds

--- lib/rates/data/exchanges/Binance.ts
+++ lib/rates/data/exchanges/Binance.ts
@@ -4,8 +4,18 @@ class Binance implements Exchange {
   private static readonly API = 'https://api.binance.com/api/v3';
 
   public async getPrice(baseAsset: string, quoteAsset: string): Promise<number> {
-    const response = await makeRequest(`${Binance.API}/ticker/price?symbol=${baseAsset.toUpperCase()}${quoteAsset.toUpperCase()}`);
-    return Number(response.price);
+    console.log('binance getPrice ', baseAsset, quoteAsset);
+    quoteAsset = quoteAsset ? 'USD' : 'USDT'
+    if (baseAsset === 'CELO' && quoteAsset === 'BTC') {
+      // get btcusdt and celousdt and divide
+      const response1 = await makeRequest(`${Binance.API}/ticker/price?symbol=CELOUSDT`);
+      const response2 = await makeRequest(`${Binance.API}/ticker/price?symbol=BTCUSDT`);
+      return Number(response1.price/response2.price);
+    } else {
+      const response = await makeRequest(`${Binance.API}/ticker/price?symbol=${baseAsset.toUpperCase()}${quoteAsset.toUpperCase()}`);
+      return Number(response.price);
+    }
+
   }
 }
 
--- lib/rates/data/exchanges/CoinbasePro.ts
+++ lib/rates/data/exchanges/CoinbasePro.ts
@@ -4,8 +4,18 @@ class CoinbasePro implements Exchange {
   private static readonly API = 'https://api.pro.coinbase.com';
 
   public async getPrice(baseAsset: string, quoteAsset: string): Promise<number> {
-    const response = await makeRequest(`${CoinbasePro.API}/products/${baseAsset}-${quoteAsset}/ticker`);
-    return Number(response.price);
+    console.log('1CoinbasePro getPrice ', baseAsset, quoteAsset);
+    baseAsset = baseAsset === 'CELO' ? 'CGLD' : baseAsset
+    console.log('2CoinbasePro getPrice ', baseAsset, quoteAsset);
+    if(baseAsset === 'CELO' && quoteAsset === 'BTC') {
+      const response1 = await makeRequest(`${CoinbasePro.API}/products/CGLD-USD/ticker`);
+      const response2 = await makeRequest(`${CoinbasePro.API}/products/BTC-USD/ticker`);
+      return Number(response1.price/response2.price);
+    } else {
+      const response = await makeRequest(`${CoinbasePro.API}/products/${baseAsset}-${quoteAsset}/ticker`);
+      return Number(response.price);
+    }
+
   }
 }
 
