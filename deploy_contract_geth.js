71999082b21ee8034dfd603ad25bf6fa8d83ef50
pseudozach <git@pseudozach.com>
2022-03-02 08:39:49 +0100

    update gitpod

--- deploy_contract_geth.js
+++ deploy_contract_geth.js
@@ -1,3 +1,3 @@
 web3.eth.defaultAccount = eth.accounts[0]
-loadScript("/workspace/lnsovbridge/erc20swap.js")
-loadScript("/workspace/lnsovbridge/etherswap.js")
+loadScript("/workspace/lncelobridge/erc20swap.js")
+loadScript("/workspace/lncelobridge/etherswap.js")
