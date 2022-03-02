69b343d422625c5c5f14aee39e63f87290240fd4
pseudozach <git@pseudozach.com>
2022-03-02 08:26:59 +0100

    add grpc cert gen script

---
+++ generategrpccerts.sh
@@ -0,0 +1,15 @@
+    mypass="pass123"
+
+    echo Generate server key:
+    openssl genrsa -passout pass:$mypass -des3 -out server.key 4096
+
+    echo Generate server signing request:
+    openssl req -passin pass:$mypass -new -key server.key -out server.csr -subj  "/C=US/ST=CA/L=SanFrancisco/O=Google/OU=youtube/CN=localhost"
+
+    echo Self-sign server certificate:
+    openssl x509 -req -passin pass:$mypass -days 365 -in server.csr -signkey server.key -set_serial 01 -out server.crt
+
+    echo Remove passphrase from server key:
+    openssl rsa -passin pass:$mypass -in server.key -out server.key
+
+    rm server.csr
